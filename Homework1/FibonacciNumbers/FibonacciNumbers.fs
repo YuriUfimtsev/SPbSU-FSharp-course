9a484b607bbd0a01b34fbcedb53dc5f38a89c5ec
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-02-19 23:04:31 +0300

    refactor: rename nested function

--- Homework1/FibonacciNumbers/FibonacciNumbers.fs
+++ Homework1/FibonacciNumbers/FibonacciNumbers.fs
@@ -2,9 +2,9 @@
     if numberInSequence <= 0 then 0
     elif numberInSequence = 1 then 1
     else
-        let rec support first second i =
+        let rec loop first second i =
             if i = numberInSequence then first
-            else support (second) (first + second) (i + 1)
-        support 0 1 0
+            else loop (second) (first + second) (i + 1)
+        loop 0 1 0
 
 printfn "The twentieth Fibonacci number: %d" (calculateFibonacciNumber 20)
