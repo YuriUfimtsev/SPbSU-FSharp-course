6ed07704cb727e4f6a4e42a13fdeef5f0a82c558
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-02-19 22:11:33 +0300

    initial commit

---
+++ Homework1/FibonacciNumbers/FibonacciNumbers.fs
@@ -0,0 +1,10 @@
+﻿let calculateFibonacciNumber numberInSequence =
+    if numberInSequence <= 0 then 0
+    elif numberInSequence = 1 then 1
+    else
+        let rec support first second i =
+            if i = numberInSequence then first
+            else support (second) (first + second) (i + 1)
+        support 0 1 0
+
+printfn "The twentieth Fibonacci number: %d" (calculateFibonacciNumber 20)
