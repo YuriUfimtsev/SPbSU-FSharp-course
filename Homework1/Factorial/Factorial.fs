f09f9041d76bc393f4060c48b276bf32d09bd595
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-02-19 21:21:14 +0300

    initial commit

---
+++ Homework1/Factorial/Factorial.fs
@@ -0,0 +1,11 @@
+﻿printfn "Enter the number"
+let data = System.Console.ReadLine()
+let number = int data
+
+let factorial number =
+    let rec support x acc = 
+        if x = 0 then acc
+        else support (x - 1) (x * acc)
+    support number 1
+
+printfn "Result: %d" (factorial number)
