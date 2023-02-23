bd2028c829349420ff9248119e91ab677df73edf
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-02-23 14:11:49 +0300

    initial commit

---
+++ Homework1/PowerRow/PowerRow.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+printfn "Enter n:"
+let n = int (Console.ReadLine())
+printfn "Enter m:"
+let m = int (Console.ReadLine())
+
+let createList n m =
+    let twoInMaxDegree = int (2.0 ** int (n + m))
+    let rec loop ls lsElement i =
+        if i = 0 then ls
+        else loop (lsElement :: ls) (lsElement / 2) (i - 1)
+
+    loop [] twoInMaxDegree (m + 1)
+
+printfn "Result: %A" (createList n m)
+
