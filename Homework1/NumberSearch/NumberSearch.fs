1ff61299eff298f4900df9a4a9a8d7bdf76ce8d5
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-02-23 15:24:55 +0300

    initial commit

---
+++ Homework1/NumberSearch/NumberSearch.fs
@@ -0,0 +1,31 @@
+﻿open System
+
+let readList lsLength =
+    let rec loop lsLength acc i =
+        if i >= lsLength then acc
+        else loop lsLength ((Console.ReadLine() |> int) :: acc) (i + 1)
+    loop lsLength [] 0
+
+let revertList initialLs =
+    let rec loop initialLs acc =
+        match initialLs with
+        | head::tail -> loop tail (head::acc)
+        | [] -> acc
+    loop initialLs []
+
+let findInList ls requestedElement =
+    let rec loop ls requestedElement i =
+        match ls with
+        | head::tail ->
+            if head = requestedElement then i
+            else loop tail requestedElement (i + 1)
+        | [] -> -1
+    loop ls requestedElement 0
+
+printfn "Enter the length of list:"
+let lsLength = Console.ReadLine() |> int
+printfn "Enter list elements:"
+let ls = readList lsLength |> revertList
+printfn "Enter the element that you want to try to find in the list:"
+let requestedElement = Console.ReadLine() |> int
+printfn "Element index in the list: %d" (findInList ls requestedElement)
