c6f6f55d75cefc13c74d92f5a98aaa533db68a6c
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-03-12 17:53:05 +0300

    initial commit

---
+++ Homework2/ParseTree/ParseTree.fs
@@ -0,0 +1,24 @@
+﻿namespace ParseTree
+
+module ParseTree =
+    type Operator =
+    | Addition
+    | Multiplication
+    | Subtraction
+    | Division
+
+    type Tree =
+    | Tree of Operator * Tree * Tree
+    | Tip of int
+    
+    let rec calculateExpression tree =
+        match tree with
+        | Tree (Addition, leftTree, rightTree) -> calculateExpression leftTree + calculateExpression rightTree
+        | Tree (Multiplication, leftTree, rightTree) -> calculateExpression leftTree * calculateExpression rightTree
+        | Tree (Subtraction, leftTree, rightTree) -> calculateExpression leftTree - calculateExpression rightTree
+        | Tree (Division, leftTree, rightTree) ->
+            let leftTreeResult = calculateExpression leftTree
+            let rightTreeResult = calculateExpression rightTree
+            if rightTreeResult <> 0 then calculateExpression leftTree / calculateExpression rightTree
+            else raise (nullArg("DivisionByZero"))
+        | Tip number -> number
