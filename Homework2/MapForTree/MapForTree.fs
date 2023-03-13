2125974ffddb04ecd9e0a2f759601e36d17fc9b4
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-03-13 12:38:10 +0300

    initial commit

---
+++ Homework2/MapForTree/MapForTree.fs
@@ -0,0 +1,12 @@
+﻿namespace MapForTree
+
+module MapForTree =
+
+    type Tree<'a> =
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | Tip of 'a
+
+    let rec mapForTree tree func =
+        match tree with
+        | Tree (element, firstTree, secondTree) -> Tree (func element, mapForTree firstTree func, mapForTree secondTree func)
+        | Tip element -> Tip (func element)
