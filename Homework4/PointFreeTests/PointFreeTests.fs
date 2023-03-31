4448be29b55801ef7f199d7f4fd12457349e65b5
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-03-31 20:27:42 +0300

    initial commit

---
+++ Homework4/PointFreeTests/PointFreeTests.fs
@@ -0,0 +1,17 @@
+module PointFreeTests
+
+open NUnit.Framework
+open FsCheck
+
+let sourceFunc x l = List.map (fun y -> y * x) l
+//let func x l = List.map (fun y -> y * x) l
+//let func x = List.map (fun y -> y * x)
+//let func x = List.map (fun y -> (*) y x)
+//let func x = List.map << (fun y -> (*) y) <| x
+//let func = List.map << (fun y -> (*) y)
+let func = List.map << (*)
+
+[<Test>]
+let ``Are the two functions the same`` () =
+    let isFuncCorrect x ls = func x ls = sourceFunc x ls
+    Check.QuickThrowOnFailure isFuncCorrect
