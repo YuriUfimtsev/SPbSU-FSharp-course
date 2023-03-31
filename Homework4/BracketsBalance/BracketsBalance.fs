ce37f6395370ea70c58df6723121a563fa20f3ce
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-03-31 23:34:04 +0300

    initial commit

---
+++ Homework4/BracketsBalance/BracketsBalance.fs
@@ -0,0 +1,25 @@
+﻿namespace BracketsBalance
+
+open Stack;
+
+module BracketsBalance =
+    let closingBrackets = Map [ (')', '('); (']', '['); ('}', '{') ];
+    let openingBrackets = Set ( seq {'('; '['; '{' } );
+
+    let checkBracketsBalance string =
+        let stack = []
+        let rec loop i isCorrect stack =
+            if not isCorrect then false
+            elif i = String.length string - 1 then isCorrect
+            else
+                let symbol = string[i]
+                if Set.contains symbol openingBrackets then
+                    loop (i + 1) isCorrect <| Stack.push stack symbol
+                elif Map.containsKey symbol closingBrackets then
+                    if Stack.isEmpty stack || Option.defaultValue ' ' (Stack.top stack) <> Map.find symbol closingBrackets
+                    then false
+                    else
+                        loop (i + 1) isCorrect <| Stack.pop stack
+                else
+                    loop (i + 1) isCorrect stack
+        if not (Stack.isEmpty stack) then false else true
---
+++ Homework4/BracketsBalance/Stack.fs
@@ -0,0 +1,20 @@
+﻿namespace BracketsBalance
+
+module Stack =
+    let push stack element = 
+        element :: stack
+    
+    let pop stack =
+        match stack with
+        | head :: tail -> tail
+        | [] -> []
+
+    let top stack =
+        match stack with
+        | head :: tail -> Some(head)
+        | [] -> None
+
+    let isEmpty stack =
+        match stack with
+        | head :: tail -> false
+        | [] -> true
