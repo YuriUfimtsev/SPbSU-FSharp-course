2ea1a78c497510cc50fc491ef631e5a5020d082f
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-04-02 16:10:11 +0300

    feat: add implementation

---
+++ Homework4/PhoneBook/PhoneBook.fs
@@ -0,0 +1,54 @@
+﻿module PhoneBook
+
+open System.IO
+
+type Person = { Name : string; Phone : string }
+
+let addRecord person database = person :: database
+
+let findName phone database =
+    let person = List.tryFind (fun x -> phone = x.Phone) database
+    match person with
+    | Some value -> Some value.Name
+    | None -> None
+
+let findPhones name database =
+    database |> List.filter (fun x -> name = x.Name) |> List.map (fun x -> x.Phone)
+
+let saveInFile (path : string) database =
+    use streamWriter = new StreamWriter(path)
+
+    let rec loop i localDatabase =
+        if i >= List.length database then
+            ()
+        else
+            match localDatabase with
+            | head :: tail ->
+                streamWriter.WriteLine(
+                    String.concat
+                        " "
+                        (seq {
+                            head.Name
+                            head.Phone
+                        })
+                )
+
+                loop (i + 1) tail
+            | [] -> ()
+
+    loop 0 database
+
+let getFromFile (path : string) =
+    use streamReader = new StreamReader(path)
+
+    let rec loop (newLine : string) database =
+        if newLine = null then
+            database
+        else
+            let array = newLine.Split ' '
+            if Array.length array <> 2 then
+                raise (invalidArg "Name and phone" "More than two parameters were found")
+            let newRecord = { Name = array[0]; Phone = array[1] }
+            loop (streamReader.ReadLine()) (newRecord :: database)
+
+    loop (streamReader.ReadLine()) []
--- Homework4/PhoneBook/Program.fs
+++ Homework4/PhoneBook/Program.fs
@@ -1,2 +1,80 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module Program
+
+open System
+open PhoneBook
+
+let printInfo =
+    printfn "Below you can find the available commands:"
+    printfn "0 - exit;"
+    printfn "1 - make a new entry (enter the name and phone number);"
+    printfn "2 - find the phone number(s) of person (enter the person's name);"
+    printfn "3 - find a person's name by phone number (enter the person's phone number);"
+    printfn "4 - fill in the database from the file (enter the path to the file);"
+    printfn "5 - save all entries in file (enter the path to the file);"
+    printfn "6 - show all database contents;"
+
+let printDatabase database =
+    let rec loop localDatabase =
+        match localDatabase with
+        | head :: tail ->
+            printfn "Name:%s; Phone:%s;" head.Name head.Phone
+            loop tail
+        | [] -> ()
+
+    loop database
+
+let printPhoneNumbers phones =
+    let rec loop localPhones =
+        match localPhones with
+        | head :: tail ->
+            printfn "%s, " head
+            loop tail
+        | [] -> ()
+
+    match List.length phones with
+    | 0 -> printfn "There is no such record in the database"
+    | _ ->
+        printf "Phones: "
+        loop phones
+
+let rec loop database =
+    printfn "Enter the next command:"
+
+    try
+        let command = Console.ReadLine() |> int
+
+        match command with
+        | 0 -> ()
+        | 1 ->
+            printfn "Enter the name: "
+            let name = Console.ReadLine()
+            printfn "Enter the phone: "
+            let phone = Console.ReadLine()
+            let person = { Name = name; Phone = phone }
+            addRecord person database |> loop
+        | 2 ->
+            (Console.ReadLine() |> findPhones <| database) |> printPhoneNumbers
+            loop database
+        | 3 ->
+            let name = Console.ReadLine() |> findName <| database
+            match name with
+            | Some value -> printfn "Name: %s" value
+            | None -> printfn "There is no such record in the database"
+            loop database
+        | 4 -> Console.ReadLine() |> getFromFile |> loop
+        | 5 ->
+            Console.ReadLine() |> saveInFile <| database
+            loop database
+        | 6 ->
+            printDatabase database
+            loop database
+        | _ ->
+            printfn "Incorrect command. Try again"
+            loop database
+    with :? FormatException ->
+        printfn "Incorrect command. Try again"
+        loop database
+
+printfn "Welcome to the phone book!"
+printInfo
+loop []
