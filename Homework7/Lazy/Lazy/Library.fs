74b9fb61acb1c027023fa5fb8aac7bbe5d7de4a3
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-05-14 10:50:19 +0300

    feat: add a couple of implementations

--- Homework7/Lazy/Lazy/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Lazy
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
---
+++ Homework7/Lazy/Lazy/MultiThreadedLazy.fs
@@ -0,0 +1,23 @@
+﻿namespace Lazy
+
+type MultiThreadedLazy<'a> (supplier : unit -> 'a) =
+    let mutable privateSupplier: Option<unit -> 'a> = Some supplier
+
+    [<VolatileField>]
+    let mutable result: Option<'a> = None
+    let synchronizationObject = obj
+
+    interface ILazy<'a> with
+        override _.Get () =
+            match result with
+            | None ->
+                lock synchronizationObject (fun () ->
+                    match result with
+                    | None ->
+                        let supplier = privateSupplier |> Option.get
+                        let value = supplier ()
+                        privateSupplier <- None
+                        result <- Some value
+                        value
+                    | Some value -> value)
+            | Some value -> value
