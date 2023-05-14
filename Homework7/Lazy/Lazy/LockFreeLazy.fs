3542ffd876e82b40d5ae4bbea18da57b7e66c3b4
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-05-14 11:16:43 +0300

    feat: add third implementation

--- Homework7/Lazy/Lazy/LockFreeLazy.fs
+++ Homework7/Lazy/Lazy/LockFreeLazy.fs
@@ -1,16 +1,24 @@
 ﻿namespace Lazy
 
+open System.Threading
+
 type LockFreeLazy<'a> (supplier : unit -> 'a) =
     let mutable privateSupplier: Option<unit -> 'a> = Some supplier
+
     let mutable result: Option<'a> = None
 
-    interface ILazy<'a> with
-        override _.Get () =
-            match result with
+    let rec get () =
+        match result with
             | None ->
-                let supplier = privateSupplier |> Option.get
-                let value = supplier ()
-                privateSupplier <- None
-                result <- Some value
-                value
+                    let currentResult = result
+                    let supplier = privateSupplier |> Option.get
+                    let value = supplier ()
+                    if obj.ReferenceEquals
+                        (result, Interlocked.CompareExchange(&result, Some value, currentResult)) |> not
+                        then get ()
+                        else result |> Option.get
             | Some value -> value
+
+    interface ILazy<'a> with
+        override this.Get () =
+            get ()
